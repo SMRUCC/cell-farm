db76fd60de39518862e4d54023cce088bb29e66d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-02 21:37:31 +0800

    define the commandline

--- src/VirtualCell/Program.vb
+++ src/VirtualCell/Program.vb
@@ -1,8 +1,15 @@
-Imports System
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
 Module Program
 
     Public Function Main(args As String()) As Integer
-        Console.WriteLine("Hello World!")
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("--run")>
+    <Usage("--run <model.xml/model-list.txt> [--extracellular <default=Extracellular> --time <default=2500> --resolution 5000 --output <result.dat>]")>
+    Public Function Run(args As CommandLine) As Integer
+
     End Function
 End Module
