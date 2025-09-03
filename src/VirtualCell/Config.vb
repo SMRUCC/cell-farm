2cb7a9e34dc1d22bccdebd9ede68c3a946f39a7b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-03 11:38:37 +0800

    fix of the missing component reference

---
+++ src/VirtualCell/Config.vb
@@ -0,0 +1,12 @@
+﻿Imports SMRUCC.genomics.GCModeller.ModellingEngine.BootstrapLoader.Definitions
+
+Public Class Config
+
+    ''' <summary>
+    ''' the file path to the model files, usually be the relative path to the config file
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property models As String()
+    Public Property mapping As Definition
+
+End Class
