87fb65aed4ba6d25bec74bc26f89e264fd9268e1
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-03 14:27:22 +0800

    update docs

--- src/VirtualCell/Config.vb
+++ src/VirtualCell/Config.vb
@@ -3,11 +3,21 @@
 Public Class Config
 
     ''' <summary>
-    ''' the file path to the model files, usually be the relative path to the config file
+    ''' the file path to the model files, usually be the relative path to this config file
     ''' </summary>
     ''' <returns></returns>
     Public Property models As String()
+    ''' <summary>
+    ''' some specific biological process in the runtime compiler required some metabolite 
+    ''' for links, example as ATP, O2, H2O. 
+    ''' this config value provides the compound id mapping to such special metabolites
+    ''' </summary>
+    ''' <returns></returns>
     Public Property mapping As Definition
+    ''' <summary>
+    ''' parameter for configs the biological process kinetic behaviour
+    ''' </summary>
+    ''' <returns></returns>
     Public Property kinetics As FluxBaseline
 
 End Class
