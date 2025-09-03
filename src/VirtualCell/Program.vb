4bd9f3808d5d6b17a85f611d3c990fae84dbbefc
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-03 11:53:15 +0800

    load config file

--- src/VirtualCell/Program.vb
+++ src/VirtualCell/Program.vb
@@ -1,5 +1,6 @@
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module Program
 
@@ -8,8 +9,11 @@ Module Program
     End Function
 
     <ExportAPI("--run")>
-    <Usage("--run <model.xml/model-list.txt> [--extracellular <default=Extracellular> --time <default=2500> --resolution 5000 --output <result.dat>]")>
+    <Usage("--run <config.json> [--time <default=2500> --resolution 5000 --output <result.dat>]")>
     Public Function Run(args As CommandLine) As Integer
+        Dim config_file As String = args.Tokens(1)
+        Dim output As String = args("--output") Or config_file.ChangeSuffix(".dat")
+        Dim config As Config = config_file.LoadJsonFile(Of Config)
 
     End Function
 End Module
