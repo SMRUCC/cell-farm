5ec14bf7be1a187ba53a9c6c0434966bb1941b41
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-24 21:11:35 +0800

    make a helper fincton for load cellular graph

--- src/VirtualCell/Program.vb
+++ src/VirtualCell/Program.vb
@@ -4,6 +4,7 @@ Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.MIME.application.json
 Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.BootstrapLoader
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.BootstrapLoader.Definitions
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.BootstrapLoader.Engine
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.BootstrapLoader.ModelLoader
@@ -59,10 +60,7 @@ Module Program
             .ParseJson _
             .CreateObject(Of Config)
         Dim model As VirtualCell = Nothing
-        Dim massTable As New MassTable
         Dim modelData As CellularModule
-        Dim processList As New List(Of Channel)
-        Dim cellular_id As New List(Of String)
         Dim modelList As New List(Of CellularModule)
 
         If output.ExtensionSuffix <> "vcelldata" Then
@@ -73,8 +71,9 @@ Module Program
             Return 404
         End If
 
+        Dim cellular_id As New List(Of String)
         Dim symbolNames As New Dictionary(Of String, String)
-        Dim fluxIndex As New Dictionary(Of String, List(Of String))
+        Dim modelDataList As New List(Of CellularModule)
 
         For Each name As String In config.models
             If Not name.FileExists Then
@@ -87,31 +86,17 @@ Module Program
 
             model = name.LoadXml(Of VirtualCell)
             modelData = model.CreateModel
-            cellular_id.Add(modelData.CellularEnvironmentName)
             modelList.Add(modelData)
-            massTable.SetDefaultCompartmentId(modelData.CellularEnvironmentName)
-
-            Dim loader As New Loader(config.mapping, config.kinetics, massTable:=massTable)
+            cellular_id.Add(modelData.CellularEnvironmentName)
 
             For Each idName As KeyValuePair(Of String, String) In model.GetMetaboliteSymbolNames
                 symbolNames(idName.Key) = idName.Value
             Next
 
-            With loader.CreateEnvironment(modelData)
-                Call processList.AddRange(.processes)
-            End With
-
-            Dim modelFluxIndex = loader.GetFluxIndex
-
-            For Each part_key As String In modelFluxIndex.Keys
-                If Not fluxIndex.ContainsKey(part_key) Then
-                    Call fluxIndex.Add(part_key, New List(Of String))
-                End If
-
-                Call fluxIndex(part_key).AddRange(modelFluxIndex(part_key))
-            Next
+            Call modelDataList.Add(modelData)
         Next
 
+        Dim pull = SyntheticMicrobialNetwork.CreateNetwork(modelDataList, config.mapping, config.kinetics)
         Dim engine As New Engine(config.mapping, config.kinetics, cellular_id.ToArray,
                                  config.iterations,
                                  config.resolution,
@@ -119,7 +104,7 @@ Module Program
                                  config.debug) With {
             .models = modelList _
                 .ToArray,
-            .fluxIndex = fluxIndex _
+            .fluxIndex = pull.fluxIndex _
                 .ToDictionary(Function(a) a.Key,
                               Function(a)
                                   Return a.Value.ToArray
@@ -127,7 +112,7 @@ Module Program
         }
 
         Call engine _
-            .SetModel(massTable, processList) _
+            .SetModel(pull.mass, pull.network) _
             .MakeKnockout(config.knockouts) _
             .SetCultureMedium(config.cultureMedium)
 
