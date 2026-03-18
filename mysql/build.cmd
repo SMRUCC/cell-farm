9ab8129cef8dcebd04b7e83b3a1ae10448cc7b12
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-18 09:56:25 +0800

    add mysql models

--- mysql/build.cmd
+++ mysql/build.cmd
@@ -1,8 +1,8 @@
 REM @echo off
 
 set reflector="\graphQL\src\mysqli\App\Reflector.exe"
-set R_src="../CellRender\MySql\cad_lab"
+set R_src="../src/CellaLab\MySql\cellaLab"
 
-%reflector% --reflects /sql ./cad_lab.sql -o %R_src% /namespace biocad_labModel --language visualbasic /split /auto_increment.disable
+%reflector% --reflects /sql ./cad_lab.sql -o %R_src% /namespace cellaLab_model --language visualbasic /split /auto_increment.disable
 
 REM pause
---
+++ src/CellaLab/MySql/cellaLab/db_cellaLab.vb
@@ -0,0 +1,36 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Namespace cellaLab_model
+
+Public MustInherit Class db_cellaLab : Inherits IDatabase
+Protected ReadOnly m_community As TableModel(Of community)
+Protected ReadOnly m_community_formula As TableModel(Of community_formula)
+Protected ReadOnly m_culture_medium As TableModel(Of culture_medium)
+Protected ReadOnly m_dynamics As TableModel(Of dynamics)
+Protected ReadOnly m_experiment As TableModel(Of experiment)
+Protected ReadOnly m_model_library As TableModel(Of model_library)
+Protected ReadOnly m_model_variant As TableModel(Of model_variant)
+Protected ReadOnly m_project_data As TableModel(Of project_data)
+Protected ReadOnly m_substrate_composition As TableModel(Of substrate_composition)
+Protected ReadOnly m_substrate_modification As TableModel(Of substrate_modification)
+Protected ReadOnly m_variants As TableModel(Of variants)
+Protected Sub New(mysqli As ConnectionUri)
+Call MyBase.New(mysqli)
+
+Me.m_community = model(Of community)()
+Me.m_community_formula = model(Of community_formula)()
+Me.m_culture_medium = model(Of culture_medium)()
+Me.m_dynamics = model(Of dynamics)()
+Me.m_experiment = model(Of experiment)()
+Me.m_model_library = model(Of model_library)()
+Me.m_model_variant = model(Of model_variant)()
+Me.m_project_data = model(Of project_data)()
+Me.m_substrate_composition = model(Of substrate_composition)()
+Me.m_substrate_modification = model(Of substrate_modification)()
+Me.m_variants = model(Of variants)()
+End Sub
+End Class
+
+End Namespace
