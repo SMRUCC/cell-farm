cc33c7df8a12b3b8eec5d5d0eb08374265fcb84b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-03 11:52:11 +0800

    save virtual cell model into muysql?

---
+++ src/CellaLab/GEMs/SaveModel.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+
+Module SaveModel
+
+    <Extension>
+    Public Sub SaveModelData(cella As CellaLAB, model As VirtualCell)
+
+    End Sub
+End Module
