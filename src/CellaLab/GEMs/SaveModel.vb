df3afb0c960e55fca35feef032c5f34e0d4b79ed
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-03 16:25:48 +0800

    run msbuild

--- src/CellaLab/GEMs/SaveModel.vb
+++ src/CellaLab/GEMs/SaveModel.vb
@@ -3,6 +3,14 @@ Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
 
 Module SaveModel
 
+    ''' <summary>
+    ''' Save virtual cell model into the model library table
+    ''' </summary>
+    ''' <param name="cella"></param>
+    ''' <param name="model"></param>
+    ''' <remarks>
+    ''' 这个函数只是把模型的摘要数据保存进入数据库中，并非将完整的模型数据和所有的参数细节都保存进入数据库中
+    ''' </remarks>
     <Extension>
     Public Sub SaveModelData(cella As CellaLAB, model As VirtualCell)
 
